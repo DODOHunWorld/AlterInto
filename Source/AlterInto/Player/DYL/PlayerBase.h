5212805dea604c9a043d971d3633e8fa11b66b82
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-05-02 19:46:50 +0900

    [feature] 기본 이동 key binding 완료

--- Source/AlterInto/Player/DYL/PlayerBase.h
+++ Source/AlterInto/Player/DYL/PlayerBase.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -12,18 +10,84 @@ class ALTERINTO_API APlayerBase : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	APlayerBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// Getters & Setters
+	bool GetCanDash();
+
+protected:
+	/* IMC */
+    UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* imcPlayer;
+
+
+	/* IA - Movement */
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* iaMove;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* iaTurn;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* iaJump;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* iaDash;
+
+
+	/* Movement - Variables */
+	// Speed
+	UPROPERTY(EditDefaultsOnly, Category = "Move|Default")
+	float playerSpeed = 600;
+
+	// Turn
+	UPROPERTY(EditDefaultsOnly, Category = "Move|Default")
+	float minPitch = -78.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Move|Default")
+	float maxPitch = 50.f;
+
+	// Jump
+	bool bIsFalling = false;
+
+	int32 playerJumpCurrentCount;
+
+	// Dash
+	bool bCanDash = false;
+
+	FVector prevPos;
+	FVector dashStartPos;
+	FVector dashEndPos;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Move|Dash")
+	float dashDistance = 300.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Move|Dash")
+	float dashCurrentTime = 0.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Move|Dash")
+	float dashDurationTime = 0.2f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Move|Dash")
+	float dashCoolDownTime = 0.5f;
+
+
+	/* Movement - Functions */
+	void DoMove(const struct FInputActionValue& InValue);
+
+	void DoTurn(const struct FInputActionValue& InValue);
+
+	void DoJump(const struct FInputActionValue& InValue);
+
+	/*virtual */void StartDash(const struct FInputActionValue& InValue);
+	void DoDash(float InDeltaTime);
+	void ResetDash(float InDeltaTime);
 };
