1e810500248883d9429e539019cf8fdf551c3b2c
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-05-09 19:01:59 +0900

    [feature] 플레이어 키 바인딩 완료

--- Source/AlterInto/Player/DYL/PlayerBase.cpp
+++ Source/AlterInto/Player/DYL/PlayerBase.cpp
@@ -23,9 +23,6 @@ APlayerBase::APlayerBase()
     cameraComp->SetupAttachment(springArmComp);
 	cameraComp->SetRelativeRotation(cameraRotationDefault);
 	cameraComp->bUsePawnControlRotation = false;
-
-
-    /* Camera - Scene Capture */
 	
 
 	/* IMC */
@@ -34,8 +31,6 @@ APlayerBase::APlayerBase()
 
 
 	/* IA - Movement */
-	//ConstructorHelpers::FObjectFinder<UInputAction> tmp(TEXT(""));
-	//if (tmp.Succeeded()) ia = tmp.Object;
 	ConstructorHelpers::FObjectFinder<UInputAction> tmpMove(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Move.IA_Move'"));
 	if (tmpMove.Succeeded()) iaMove = tmpMove.Object;
 
@@ -45,8 +40,36 @@ APlayerBase::APlayerBase()
 	ConstructorHelpers::FObjectFinder<UInputAction> tmpJump(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Jump.IA_Jump'"));
 	if (tmpJump.Succeeded()) iaJump = tmpJump.Object;
 
-	ConstructorHelpers::FObjectFinder<UInputAction> tmpDash(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Dash.IA_Dash'"));
-	if (tmpDash.Succeeded()) iaDash = tmpDash.Object;
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpDash(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Dodge.IA_Dodge'"));
+	if (tmpDash.Succeeded()) iaDodge = tmpDash.Object;
+
+
+	/* IA - Combat */
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpAttack(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Attack.IA_Attack'"));
+	if (tmpAttack.Succeeded()) iaAttack = tmpAttack.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpSkillE(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_SkillE.IA_SkillE'"));
+	if (tmpSkillE.Succeeded()) iaSkillE = tmpSkillE.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpSkillQ(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_SkillQ.IA_SkillQ'"));
+	if (tmpSkillQ.Succeeded()) iaSkillQ = tmpSkillQ.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpGuard(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Guard.IA_Guard'"));
+	if (tmpGuard.Succeeded()) iaGuard = tmpGuard.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpLock(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_LockOn.IA_LockOn'"));
+	if (tmpLock.Succeeded()) iaLockOn = tmpLock.Object;
+
+
+	/* IA - Etc */
+	ConstructorHelpers::FObjectFinder<UInputAction> tmp1(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Player1.IA_Player1'"));
+	if (tmp1.Succeeded()) iaPlayer1 = tmp1.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmp2(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_Player2.IA_Player2'"));
+	if (tmp2.Succeeded()) iaPlayer2 = tmp2.Object;
+
+	ConstructorHelpers::FObjectFinder<UInputAction> tmpPet(TEXT("/Script/EnhancedInput.InputAction'/Game/Player/DYL/Inputs/IA_RidePet.IA_RidePet'"));
+	if (tmpPet.Succeeded()) iaRidePet = tmpPet.Object;
 
 
     /* Movement - Rotation */
@@ -69,15 +92,17 @@ void APlayerBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	bIsLockedOn = false;
+	bIsRidingPet = false;
 }
 
 void APlayerBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	/* Dash */
-    if (bIsDashing) DoDash(DeltaTime);
-    if (bCanResetDash) ResetDash(DeltaTime);
+	/* Dodge */
+    if (bIsDodging) DoDodge(DeltaTime);
+    if (bCanResetDodge) ResetDodge(DeltaTime);
 }
 
 void APlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -102,18 +127,30 @@ void APlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 		input->BindAction(iaMove, ETriggerEvent::Triggered, this, &APlayerBase::DoMove);
 		input->BindAction(iaTurn, ETriggerEvent::Triggered, this, &APlayerBase::DoTurn);
 		input->BindAction(iaJump, ETriggerEvent::Started, this, &APlayerBase::DoJump);
-		input->BindAction(iaDash, ETriggerEvent::Started, this, &APlayerBase::StartDash);
+		input->BindAction(iaDodge, ETriggerEvent::Started, this, &APlayerBase::StartDodge);
+
+		// Combat
+		input->BindAction(iaAttack, ETriggerEvent::Started, this, &APlayerBase::Attack);
+		input->BindAction(iaSkillE, ETriggerEvent::Started, this, &APlayerBase::SkillE);
+		input->BindAction(iaSkillQ, ETriggerEvent::Started, this, &APlayerBase::SkillQ);
+		input->BindAction(iaGuard, ETriggerEvent::Started, this, &APlayerBase::Guard);
+		input->BindAction(iaLockOn, ETriggerEvent::Started, this, &APlayerBase::LockOnOff);
+
+		// Etc
+		input->BindAction(iaPlayer1, ETriggerEvent::Started, this, &APlayerBase::ChangeCharacter1);
+		input->BindAction(iaPlayer2, ETriggerEvent::Started, this, &APlayerBase::ChangeCharacter2);
+		input->BindAction(iaRidePet, ETriggerEvent::Started, this, &APlayerBase::RidePetOnOff);
 	}
 }
 
-bool APlayerBase::GetCanDash()
+bool APlayerBase::GetIsDodging()
 {
-	return bIsDashing;
+	return bIsDodging;
 }
 
 void APlayerBase::DoMove(const struct FInputActionValue& InValue)
 {
-    if (bIsDashing || bIsDead) return;
+    if (bIsDodging || bIsDead) return;
 
     FVector2d move = InValue.Get<FVector2d>();
     AddMovementInput(cameraComp->GetForwardVector() * move.X + cameraComp->GetRightVector() * move.Y);
@@ -139,42 +176,99 @@ void APlayerBase::DoJump(const struct FInputActionValue& InValue)
 	playerJumpCurrentCount = JumpCurrentCount;
 }
 
-void APlayerBase::StartDash(const struct FInputActionValue& InValue)
+void APlayerBase::StartDodge(const struct FInputActionValue& InValue)
 {
-	//UE_LOG(LogTemp, Warning, TEXT(">>> dash"));
-    if (bIsDead || bIsDashing || bCanResetDash) return;
+	//UE_LOG(LogTemp, Warning, TEXT(">>> Dodge"));
+    if (bIsDead || bIsDodging || bCanResetDodge) return;
 
-    dashStartPos = GetActorLocation();
-    dashEndPos = GetActorLocation() + GetActorForwardVector() * dashDistance;
-    bIsDashing = true;
-    bCanResetDash = false;
+    dodgeStartPos = GetActorLocation();
+    dodgeEndPos = GetActorLocation() + GetActorForwardVector() * dodgeDistance;
+    bIsDodging = true;
+    bCanResetDodge = false;
 }
 
-void APlayerBase::DoDash(float InDeltaTime)
+void APlayerBase::DoDodge(float InDeltaTime)
 {
-    dashCurrentTime += InDeltaTime;
-    if (dashCurrentTime >= dashDurationTime)
+    dodgeCurrentTime += InDeltaTime;
+    if (dodgeCurrentTime >= dodgeDurationTime)
     {
-        bIsDashing = false;
-        bCanResetDash = true;
-        dashCurrentTime = 0.f;
-        SetActorLocation(dashEndPos);
+        bIsDodging = false;
+        bCanResetDodge = true;
+        dodgeCurrentTime = 0.f;
+        SetActorLocation(dodgeEndPos);
     }
     else
     {
-        SetActorLocation( FMath::Lerp(dashStartPos, dashEndPos, dashCurrentTime / dashDurationTime) );
+        SetActorLocation( FMath::Lerp(dodgeStartPos, dodgeEndPos, dodgeCurrentTime / dodgeDurationTime) );
     }
 }
 
-void APlayerBase::ResetDash(float InDeltaTime)
+void APlayerBase::ResetDodge(float InDeltaTime)
 {
-    if (bCanResetDash)
+    if (bCanResetDodge)
     {
-        dashCurrentTime += InDeltaTime;
-        if (dashCurrentTime >= dashCoolDownTime)
+        dodgeCurrentTime += InDeltaTime;
+        if (dodgeCurrentTime >= dodgeCoolDownTime)
         {
-            bCanResetDash = false;
-            dashCurrentTime = 0.f;
+            bCanResetDodge = false;
+            dodgeCurrentTime = 0.f;
         }
     }
+}
+
+void APlayerBase::Attack(const struct FInputActionValue& InValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT(">>> Attack"));
+}
+
+void APlayerBase::SkillE(const struct FInputActionValue& InValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT(">>> SkillE"));
+}
+
+void APlayerBase::SkillQ(const struct FInputActionValue& InValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT(">>> SkillQ"));
+}
+
+void APlayerBase::Guard(const struct FInputActionValue& InValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT(">>> Guard"));
+}
+
+void APlayerBase::LockOnOff(const struct FInputActionValue& InValue)
+{
+	bIsLockedOn = !bIsLockedOn;
+	UE_LOG(LogTemp, Warning, TEXT(">>> LockOnOff - %d"), bIsLockedOn);
+}
+
+
+void APlayerBase::ChangeCharacter1(const struct FInputActionValue& InValue)
+{
+	if(bIsPlayer1) return;
+
+	UE_LOG(LogTemp, Warning, TEXT(">>> Character 1"));
+	bIsPlayer1 = true;
+	ChangeCharacter();
+}
+
+void APlayerBase::ChangeCharacter2(const struct FInputActionValue& InValue)
+{
+	if (!bIsPlayer1) return;
+
+	UE_LOG(LogTemp, Warning, TEXT(">>> Character 2"));
+	bIsPlayer1 = false;
+	ChangeCharacter();
+}
+
+void APlayerBase::ChangeCharacter()
+{
+	UE_LOG(LogTemp, Warning, TEXT(">>> ChangeCharacter"));
+}
+
+
+void APlayerBase::RidePetOnOff(const struct FInputActionValue& InValue)
+{
+	bIsRidingPet = !bIsRidingPet;
+	UE_LOG(LogTemp, Warning, TEXT(">>> RidePetOnOff - %d"), bIsRidingPet);
 }
